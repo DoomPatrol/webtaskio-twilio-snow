c51619fa9a071b1c59933fe857d5886b1d08cf4e
Austin Grandt <austin.grandt@gmail.com>
2017-10-25 17:57:23 -0500

    initial commit for snow.js

---
+++ README.md
@@ -0,0 +1,21 @@
+Webtask.IO + Twilio Example
+-------------------------
+
+A script on how to get up and running with Webtask.io and Twilio. Simply input your own Twilio keys and variables and you should be up and running soon.
+
+If you need to install Webtask, [here are some directions](https://webtask.io/cli).
+
+If you want to get started with this particular script do the following:
+
+ 1. Install Webtask
+ 2. Add your Twilio Sid and authToken to snow.js
+ 3. run `wt create snow.js`
+ 4. Webtask will create an endpoint link for you. Most likely something like https://ww-SOMETHING.run.webtask.io/snow
+ 5. Go back to Twilio console, click on the number you want to use from your account and add the endpoint from the previous step to your configuration. Make sure the method is `HTTP POST` like the picture below.
+ 
+ ![Twilio console](https://i.imgur.com/sA47wse.png)
+ 
+ 6. You should now be able to send your number a text and get a response in return.
+ 7. Edit the API calls to any endpoint and make your own text bot!
+
+ 
---
+++ snow.js
@@ -0,0 +1,77 @@
+//config for Twilio
+var cfg = {};
+
+cfg.twilioAccountId = '';
+cfg.authToken = '';
+
+
+var app = new (require('express'))();
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+var wt = require('webtask-tools');
+var request = require('request-promise@1.0.2');
+var twilio = require('twilio');
+var sms = new twilio(cfg.twilioAccountId, cfg.authToken);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/message', (req, res) => {
+  var respNum = req.body.From;
+  var smsBody = req.body.Body;
+  if (smsBody.toLowerCase() === 'hi' || smsBody.toLowerCase === 'hello') {
+    // greeting message
+    sms.messages.create({
+      body: 'Welcome to epic snow. Please type the mountain you want to shred at. Options are: ' +
+      'Vail, Beaver Creek, Afton Alps, Breckenridge, Keystone, or Kirkwood.',
+      to: respNum,
+      from: '+13123130932'
+    }).then((message) => console.log(message.sid));
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end();
+  } else {
+    // test for a mountain in the customer query
+    request.get('http://www.epicmix.com/vailresorts/sites/epicmix/api/mobile/mountains.ashx', (error, response, body) => {
+      var respData = JSON.parse(body);
+      var mountain = _.find(respData.mountains, 'name', smsBody);
+      if (mountain) {
+        request.get('http://www.epicmix.com/vailresorts/sites/epicmix/api/mobile/weather.ashx', (error, response, body) => {
+          var weatherData = JSON.parse(body);
+          var currentweather = _.find(weatherData.snowconditions, 'resortID', mountain.id);
+
+          sms.messages.create({
+            body: mountain.name + ' currently has ' +
+            currentweather.midMountainBase + ' inches of base and ' + currentweather.newSnow +
+            ' inches of new snow. Get out there and ride!',
+            mediaUrl: mountain.logoURLString,
+            to: respNum,
+            from: '+13123130932'
+          }).then((message) => console.log(message.sid));
+          res.writeHead(200, { 'Content-Type': 'text/xml' });
+          res.end();
+        });
+      } else {
+        // if no mountain is found from the API
+        sms.messages.create({
+          body: 'We were not able to find your mountain. Could you try again with exact spelling please?',
+          to: respNum,
+          from: '+13123130932'
+        }).then((message) => console.log(message.sid));
+        res.writeHead(200, { 'Content-Type': 'text/xml' });
+        res.end();
+      }
+    });
+
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Starting up the app');
+  res.end();
+});
+
+module.exports = wt.fromExpress(app).auth0({
+  exclude: [
+    '/',
+    '/message'
+  ]
+});
